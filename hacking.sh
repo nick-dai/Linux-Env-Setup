d9551584e13c7049265ebc0d28d2727ce538aff5
Nick Dai <17357414+UNicK37@users.noreply.github.com>
2018-11-22 10:04:00 +0800

    Add OneGadget.

--- hacking.sh
+++ hacking.sh
@@ -14,15 +14,14 @@ cp ~/Pwngdb/.gdbinit ~/
 
 # radare2
 git clone https://github.com/radare/radare2
-cd radare2
-sys/install.sh   # just run this script to update r2 from git
+./radare2/sys/install.sh   # just run this script to update r2 from git
 
 # https://github.com/Gallopsled/pwntools
 # For Python 2
 sudo apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential -y
 sudo pip install --upgrade pip pwntools
 # For Python 3
-sudo apt install python3 python3-dev python3-pip git -y
+sudo apt-get install python3 python3-dev python3-pip git -y
 sudo pip3 install --upgrade pip
 sudo pip3 install --upgrade git+https://github.com/arthaud/python3-pwntools.git
 
@@ -39,3 +38,7 @@ sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 -y
 # https://askubuntu.com/questions/453681/gcc-wont-link-with-m32/453687#453687
 # Compile 32-bit program with GCC
 sudo apt-get install gcc-multilib g++-multilib -y
+
+# OneGadget
+sudo apt-get install ruby -y
+sudo gem install one_gadget
